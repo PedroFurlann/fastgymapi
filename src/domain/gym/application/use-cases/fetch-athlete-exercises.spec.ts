0ba531a50e3db35db4f0c979fa6172066c8867bf
Pedro Furlan <pedrofurlan6@gmail.com>
2023-12-30 15:04:32 -0300

    test: add unit test for fetch-athlete-exercises use case

---
+++ src/domain/gym/application/use-cases/fetch-athlete-exercises.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryExerciseRepository } from '../../../../../test/repositories/in-memory-exercise-repository';
+import { makeExercise } from '../../../../../test/factories/make-exercise';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { FetchAthleteExercisesUseCase } from './fetch-athlete-exercises';
+
+let inMemoryExerciseRepository: InMemoryExerciseRepository;
+let sut: FetchAthleteExercisesUseCase;
+
+describe('Fetch coach exercises', () => {
+  beforeEach(() => {
+    inMemoryExerciseRepository = new InMemoryExerciseRepository();
+    sut = new FetchAthleteExercisesUseCase(inMemoryExerciseRepository);
+  });
+
+  it('Fetch all coach exercises by coachId', async () => {
+    await inMemoryExerciseRepository.create(
+      makeExercise({
+        athleteId: new UniqueEntityID('athlete-1'),
+      }),
+    );
+
+    await inMemoryExerciseRepository.create(
+      makeExercise({
+        athleteId: new UniqueEntityID('athlete-1'),
+      }),
+    );
+
+    const result = await sut.execute({
+      athleteId: 'athlete-1',
+    });
+
+    expect(result.value.exercises).toHaveLength(2);
+    expect(result.value.exercises).toEqual([
+      expect.objectContaining({ athleteId: new UniqueEntityID('athlete-1') }),
+      expect.objectContaining({ athleteId: new UniqueEntityID('athlete-1') }),
+    ]);
+  });
+});
