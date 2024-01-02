97e5aadb74eadf977a109ad75f263a8941cfdd0c
Pedro Furlan <pedrofurlan6@gmail.com>
2024-01-01 21:07:27 -0300

    feat: implementing env module for infra layer

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { EnvModule } from './env/env.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env/env';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
 })
 export class AppModule {}
