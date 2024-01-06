894fc4ab9c07d36566d3127bca3794a54dd957b5
Pedro Furlan <pedrofurlan6@gmail.com>
2024-01-06 18:04:06 -0300

    feat: implementing auth layer using jwt strategy

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './env/env';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { envSchema } from './env/env';
       isGlobal: true,
     }),
     EnvModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
