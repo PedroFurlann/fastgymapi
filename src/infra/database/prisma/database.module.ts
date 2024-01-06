5cd8546cf82b9d904db1a4b1d2388f4152cdd7e0
Pedro Furlan <pedrofurlan6@gmail.com>
2024-01-06 15:45:45 -0300

    feat: add prisma repositories for database module

--- src/infra/database/prisma/database.module.ts
+++ src/infra/database/prisma/database.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { CoachRepository } from '@/domain/gym/application/repositories/coach-repository';
+import { PrismaCoachRepository } from './repositories/prisma-coach-repository';
+import { AthleteRepository } from '@/domain/gym/application/repositories/athlete-repository';
+import { PrismaAthleteRepository } from './repositories/prisma-athlete-repository';
+import { ExerciseRepository } from '@/domain/gym/application/repositories/exercise-repository';
+import { PrismaExerciseRepository } from './repositories/prisma-exercise-repository';
 
 @Module({
   imports: [],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: CoachRepository,
+      useClass: PrismaCoachRepository,
+    },
+    {
+      provide: AthleteRepository,
+      useClass: PrismaAthleteRepository,
+    },
+    {
+      provide: ExerciseRepository,
+      useClass: PrismaExerciseRepository,
+    },
+  ],
+  exports: [
+    PrismaService,
+    CoachRepository,
+    AthleteRepository,
+    ExerciseRepository,
+  ],
 })
 export class DatabaseModule {}
