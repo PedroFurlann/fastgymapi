e4af5e195317a550774e2ab7b18c04fc668abeae
Pedro Furlan <pedrofurlan6@gmail.com>
2024-01-12 22:52:24 -0300

    feat: add coach role guard

---
+++ src/infra/auth/coach-role.guard.ts
@@ -0,0 +1,12 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { UserPayload } from './jwt.strategy';
+
+@Injectable()
+export class CoachRoleGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as UserPayload;
+
+    return user.isAthlete === false;
+  }
+}
