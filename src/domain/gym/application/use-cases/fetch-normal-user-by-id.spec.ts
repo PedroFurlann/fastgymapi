7b5b1a3f79c10b9d839bb077036a1c3fe9194bca
Pedro Furlan <pedrofurlan6@gmail.com>
2024-03-10 11:29:48 -0300

    test: add unit test for fetch-normal-user-by-id use case

---
+++ src/domain/gym/application/use-cases/fetch-normal-user-by-id.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryNormalUserRepository } from '../../../../../test/repositories/in-memory-normal-user-repository';
+import { makeNormalUser } from '../../../../../test/factories/make-normal-user';
+import { FetchNormalUserByIdUseCase } from './fetch-normal-user-by-id';
+
+let inMemoryNormalUserRepository: InMemoryNormalUserRepository;
+let sut: FetchNormalUserByIdUseCase;
+
+describe('Fetch normaluser', () => {
+  beforeEach(() => {
+    inMemoryNormalUserRepository = new InMemoryNormalUserRepository();
+    sut = new FetchNormalUserByIdUseCase(inMemoryNormalUserRepository);
+  });
+
+  it('Fetch normaluser by id', async () => {
+    const normalUser = makeNormalUser();
+
+    await inMemoryNormalUserRepository.create(normalUser);
+
+    const result = await sut.execute({
+      normalUserId: normalUser.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value.normalUser).toEqual(
+      expect.objectContaining({
+        name: inMemoryNormalUserRepository.items[0].name,
+      }),
+    );
+  });
+});
