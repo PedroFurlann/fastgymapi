0a8cf15904c650277f5ae06ba80cedbb119aceea
Pedro Furlan <pedrofurlan6@gmail.com>
2024-03-14 23:55:41 -0300

    feat: add /register/normal-user endpoint

--- src/infra/http/controllers/register.controller.ts
+++ src/infra/http/controllers/register.controller.ts
@@ -19,6 +19,8 @@ import { UserPayload } from '@/infra/auth/jwt.strategy';
 import { AthleteAlreadyExistsError } from '@/domain/gym/application/use-cases/errors/athlete-already-exists-error';
 import { CoachPresenter } from '../presenters/coach-presenter';
 import { AthletePresenter } from '../presenters/athlete-presenter';
+import { RegisterNormalUserUseCase } from '@/domain/gym/application/use-cases/register-normal-user';
+import { NormalUserPresenter } from '../presenters/norma-user-presenter';
 
 const createCoachBodySchema = z.object({
   name: z.string(),
@@ -27,6 +29,16 @@ const createCoachBodySchema = z.object({
 });
 
 type CreateCoachBodySchemaType = z.infer<typeof createCoachBodySchema>;
+
+const createNormalUserBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateNormalUserBodySchemaType = z.infer<
+  typeof createNormalUserBodySchema
+>;
 
 const createAthleteBodySchema = z.object({
   name: z.string(),
@@ -45,7 +57,36 @@ export class RegisterController {
   constructor(
     private readonly registerCoachUseCase: RegisterCoachUseCase,
     private readonly registerAthleteUseCase: RegisterAthleteUseCase,
+    private readonly registerNormalUserUseCase: RegisterNormalUserUseCase,
   ) {}
+
+  @Public()
+  @Post('/normal-user')
+  @UsePipes(new ZodValidationPipe(createNormalUserBodySchema))
+  async registerNormalUser(@Body() body: CreateNormalUserBodySchemaType) {
+    const { name, email, password } = body;
+
+    const result = await this.registerNormalUserUseCase.execute({
+      name,
+      email,
+      password,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case CoachAlreadyExistsError:
+          throw new ConflictException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+
+    const { normalUser } = result.value;
+
+    return { normalUser: NormalUserPresenter.toHTTP(normalUser) };
+  }
 
   @Public()
   @Post('/coach')
