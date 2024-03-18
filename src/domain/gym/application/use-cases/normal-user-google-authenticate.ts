72746df9e384c0581d5e1c36ce0970e132155d29
Pedro Furlan <pedrofurlan6@gmail.com>
2024-03-17 23:52:51 -0300

    feat: add normal-user-google-authenticate use case

---
+++ src/domain/gym/application/use-cases/normal-user-google-authenticate.ts
@@ -0,0 +1,73 @@
+import { Either, left, right } from '@/core/either';
+import { NormalUserRepository } from '../repositories/normal-user-repository';
+import { Injectable } from '@nestjs/common';
+import { HashComparer } from '../cryptography/hash-comparer';
+import { Encrypter } from '../cryptography/encrypter';
+import { WrongCredentialsError } from './errors/wrong-credentials-error';
+import { NormalUser } from '../../enterprise/entities/normal-user';
+
+interface NormalUserGoogleAuthenticateRequest {
+  name?: string;
+  email: string;
+  avatarUrl?: string;
+  password?: string;
+}
+
+type NormalUserGoogleAuthenticateResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string;
+  }
+>;
+@Injectable()
+export class NormalUserGoogleAuthenticate {
+  constructor(
+    private readonly normalUserRepository: NormalUserRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    avatarUrl,
+    password,
+  }: NormalUserGoogleAuthenticateRequest): Promise<NormalUserGoogleAuthenticateResponse> {
+    let normalUser = await this.normalUserRepository.findByEmail(email);
+
+    if (!normalUser) {
+      const createdNormalUser = NormalUser.create({
+        name,
+        email,
+        password: password ?? null,
+        avatarUrl,
+      });
+
+      await this.normalUserRepository.create(createdNormalUser);
+
+      normalUser = await this.normalUserRepository.findByEmail(email);
+    }
+
+    let isPasswordValid: boolean;
+
+    if (password) {
+      isPasswordValid = await this.hashComparer.compare(
+        password,
+        normalUser.password,
+      );
+    }
+
+    if (password && !isPasswordValid) {
+      return left(new WrongCredentialsError());
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: normalUser.id.toString(),
+      normalUser: true,
+    });
+
+    return right({
+      accessToken,
+    });
+  }
+}
