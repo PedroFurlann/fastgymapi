d6d33bb9309a4b522ed294a3385b8576b34cd43b
Pedro Furlan <pedrofurlan6@gmail.com>
2024-05-25 21:53:26 -0300

    feat: create workout entity

---
+++ src/domain/gym/enterprise/entities/workout.ts
@@ -0,0 +1,77 @@
+import { Entity } from '../../../../core/entities/entity';
+import { UniqueEntityID } from '../../../../core/entities/unique-entity-id';
+import { Optional } from '../../../../core/types/optional';
+
+export interface WorkoutProps {
+  title: string;
+  createdAt: Date;
+  updatedAt?: Date | null;
+  coachId?: UniqueEntityID | null;
+  normalUserId?: UniqueEntityID | null;
+  athleteId?: UniqueEntityID | null;
+}
+
+export class Workout extends Entity<WorkoutProps> {
+  get title() {
+    return this.props.title;
+  }
+
+  set title(newTitle: string) {
+    this.props.title = newTitle;
+    this.touch();
+  }
+
+  get coachId() {
+    return this.props.coachId;
+  }
+
+  set coachId(coachId: UniqueEntityID) {
+    this.props.coachId = coachId;
+    this.touch();
+  }
+
+  get athleteId() {
+    return this.props.athleteId;
+  }
+
+  set athleteId(athleteId: UniqueEntityID) {
+    this.props.athleteId = athleteId;
+    this.touch();
+  }
+
+  get normalUserId() {
+    return this.props.normalUserId;
+  }
+
+  set normalUserId(normalUserId: UniqueEntityID) {
+    this.props.normalUserId = normalUserId;
+    this.touch();
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(
+    props: Optional<WorkoutProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const workout = new Workout(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    );
+
+    return workout;
+  }
+}
