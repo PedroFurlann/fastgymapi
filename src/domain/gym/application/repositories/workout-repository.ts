74096c3ab930b15fb5a70c1cd1cfa1fbfafb3388
Pedro Furlan <pedrofurlan6@gmail.com>
2024-05-25 22:36:47 -0300

    feat: create workout repository

---
+++ src/domain/gym/application/repositories/workout-repository.ts
@@ -0,0 +1,13 @@
+import { Workout } from '../../enterprise/entities/workout';
+
+export abstract class WorkoutRepository {
+  abstract create(workout: Workout): Promise<void>;
+  abstract findById(workoutId: string): Promise<Workout | null>;
+  abstract update(workout: Workout): Promise<void>;
+  abstract delete(workoutId: string): Promise<void>;
+  abstract findManyByCoachId(coachId: string): Promise<Workout[] | null>;
+  abstract findManyByAthleteId(athleteId: string): Promise<Workout[] | null>;
+  abstract findManyByNormalUserId(
+    normalUserId: string,
+  ): Promise<Workout[] | null>;
+}
