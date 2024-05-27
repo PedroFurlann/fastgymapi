efeb173e6d299a172d646b423c7e30fae8b78e64
Pedro Furlan <pedrofurlan6@gmail.com>
2024-05-27 11:40:57 -0300

    feat: creating edit workout use case

---
+++ src/domain/gym/application/use-cases/edit-normal-user-workout.ts
@@ -0,0 +1,47 @@
+import { Workout } from '../../enterprise/entities/workout';
+import { Injectable } from '@nestjs/common';
+import { WorkoutRepository } from '../repositories/workout-repository';
+import { Either, left, right } from '@/core/either';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+
+interface EditNormalUserWorkoutUseCaseRequest {
+  normalUserId: string;
+  title: string;
+  category: string;
+  description: string;
+  workoutId: string;
+}
+
+type EditNormalUserWorkoutUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  { workout: Workout }
+>;
+@Injectable()
+export class EditNormalUserWorkoutUseCase {
+  constructor(private workoutRepository: WorkoutRepository) {}
+
+  async execute({
+    normalUserId,
+    title,
+    workoutId,
+  }: EditNormalUserWorkoutUseCaseRequest): Promise<EditNormalUserWorkoutUseCaseResponse> {
+    const workoutSelected = await this.workoutRepository.findById(workoutId);
+
+    if (!workoutSelected) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (workoutSelected.normalUserId.toString() !== normalUserId) {
+      return left(new NotAllowedError());
+    }
+
+    workoutSelected.title = title;
+
+    await this.workoutRepository.update(workoutSelected);
+
+    return right({
+      workout: workoutSelected,
+    });
+  }
+}
