52286b08b12ef17b0bc838eb03d6cb1595cdf0e5
Pedro Furlan <pedrofurlan6@gmail.com>
2024-05-27 15:35:03 -0300

    test: creating fetch normal user workouts use case unit test

---
+++ src/domain/gym/application/use-cases/fetch-normal-user-workouts.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryWorkoutRepository } from '../../../../../test/repositories/in-memory-workout-repository';
+import { FetchNormalUserWorkoutsUseCase } from './fetch-normal-user-workouts';
+import { makeWorkout } from '../../../../../test/factories/make-workout';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+let inMemoryWorkoutRepository: InMemoryWorkoutRepository;
+let sut: FetchNormalUserWorkoutsUseCase;
+
+describe('Fetch normal user workouts', () => {
+  beforeEach(() => {
+    inMemoryWorkoutRepository = new InMemoryWorkoutRepository();
+    sut = new FetchNormalUserWorkoutsUseCase(inMemoryWorkoutRepository);
+  });
+
+  it('Fetch all normaluser workouts by normalUserId', async () => {
+    await inMemoryWorkoutRepository.create(
+      makeWorkout({
+        normalUserId: new UniqueEntityID('normal-user-1'),
+      }),
+    );
+
+    await inMemoryWorkoutRepository.create(
+      makeWorkout({
+        normalUserId: new UniqueEntityID('normal-user-1'),
+      }),
+    );
+
+    const result = await sut.execute({
+      normalUserId: 'normal-user-1',
+    });
+
+    expect(result.value.workouts).toHaveLength(2);
+    expect(result.value.workouts).toEqual([
+      expect.objectContaining({
+        normalUserId: new UniqueEntityID('normal-user-1'),
+      }),
+      expect.objectContaining({
+        normalUserId: new UniqueEntityID('normal-user-1'),
+      }),
+    ]);
+  });
+});
